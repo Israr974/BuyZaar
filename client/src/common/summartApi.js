2f101a9301aa3d8b431251f84753797c04665727
Israr <israrisrar5530@gmai.com>
2026-04-12 00:32:11 +0530

    first commit

--- client/src/common/summartApi.js
+++ client/src/common/summartApi.js
@@ -1,8 +1,8 @@
 
 
-export const baseUrl = "http://localhost:3000";
+// export const baseUrl = "http://localhost:3000";
 
-// export const baseUrl ="https://buyzaar-1.onrender.com"
+export const baseUrl ="https://buyzaar-0ye1.onrender.com"
 
 const summaryApi = () => {
   return {
